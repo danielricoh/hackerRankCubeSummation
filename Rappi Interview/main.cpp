e74a7b3e6ae1453fa8b28f40659e31f073a89623
Daniel Rico <danieleo90@gmail.com>
2017-01-07 23:33:57 -0500

    Fixing inclusion exclusion principle on BIT

--- Rappi Interview/main.cpp
+++ Rappi Interview/main.cpp
@@ -15,13 +15,14 @@ int main(){
             if(operationName == "QUERY"){
                 int x1=0, y1=0, z1=0, x2=0, y2=0, z2=0;
                 cin>> x1 >> y1 >> z1 >> x2 >> y2 >> z2;
-                //TODO Query after applying exclusion inclusion principle
+                cout<<biTree.query(x1,y1,z1,x2,y2,z2)<<endl;
             }
             else if(operationName == "UPDATE"){
                 int x=0,y=0,z=0;
                 long long int W=0;
                 cin>> x >> y >> z >> W;
-                //TODO Update after applying exclusion inclusion principle
+                long long partialValue = biTree.query(x,y,z,x,y,z);
+                biTree.update(x,y,z,W - partialValue);
             }
         }
     }
--- Rappi Interview/src/BinaryIndexedTree.cpp
+++ Rappi Interview/src/BinaryIndexedTree.cpp
@@ -1,13 +1,16 @@
 #include "BinaryIndexedTree.h"
 #include "utils.h"
-#include<iostream>
 
 BinaryIndexedTree::BinaryIndexedTree(int n){
     biTree.resize(n+1, std::vector<std::vector<long long> >(n+1, std::vector<long long>(n+1)));
 }
 
 long long BinaryIndexedTree::query(int x1, int y1, int z1, int x2, int y2, int z2){
-    return query(x2,y2,z2) - query(x1-1,y1-1,z1-1);
+    long long part1 = query(x2,y2,z2)- query(x1-1,y2,z2) - query(x2,y1-1,z2) + query(x1-1,y1-1,z2);
+
+    long long part2 = query(x2,y2,z1-1) - query(x1-1,y2,z1-1) - query(x2,y1-1,z1-1)  + query(x1-1,y1-1,z1-1);
+
+    return part1 - part2;
 }
 void BinaryIndexedTree::update(int x, int y, int z, long long W){
     for( ; z < (int)biTree.size(); z+= Utils::LSOne(z)) {
